26cb6b04748d3c9413148a0cd17fa37b8323e352
Daniel P. Lima <daniel.plima@outlook.com>
2021-09-07 16:15:57 -0400

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://www.linkedin.com/in/daniel-pereira-lima-7bb9a823" target="_blank" rel="noreferrer">Daniel P. lima</a></p>
+                <p className="text-light"><small><strong>Spring React Weekly</strong><br /></small></p>
+            </div>
+        </footer>
+                );
+}
+
+export default Footer;
