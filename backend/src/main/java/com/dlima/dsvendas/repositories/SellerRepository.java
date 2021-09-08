f62a29804c82b1ee6c1d7415bad3b438bc8d50a4
Daniel P. Lima <daniel.plima@outlook.com>
2021-09-08 15:57:28 -0400

    Layers

---
+++ backend/src/main/java/com/dlima/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.dlima.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dlima.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
