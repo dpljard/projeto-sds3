b3c5a3cc03e2b4d1cc3e5e12814f101fbc216d97
Daniel P. Lima <daniel.plima@outlook.com>
2021-09-11 23:13:32 -0400

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -23,16 +23,8 @@ const DonutChart = () => {
             const myseries = data.map(x => x.sum);
 
             setChartData({ labels: mylabes, series: myseries});
-            console.log(chartData)
         })
     }, []);
-
-    
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
 
     const options = {
         legend: {
